55ad4191b9600c1d2f2a63193e5cf2d957e79002
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-09-15 16:32:47 +0530

    fix: output solution incompatibility

--- models/output_models.go
+++ models/output_models.go
@@ -1,30 +1,10 @@
 package models
 
-import (
-	"time"
-)
-
-type OutputTeacher struct {
-	Name       string
-	Sub        OutputSubject
-	StartShift time.Time
-	EndShift   time.Time
+type ScheduledPeriod struct {
+	ClassGroupName string
+	Subject        InputSubject
+	Teacher        InputTeacher
+	Timeslot       InputTimeslot
 }
 
-type OutputSubject struct {
-	Name  string
-	Hours int
-}
-
-type OutputTimeslot struct {
-	Start time.Time
-	End   time.Time
-}
-
-type Day struct {
-	Periods []OutputTimeslot
-}
-
-type OutputTimetable struct {
-	Week []Day
-}
+type TimetableSolution []ScheduledPeriod
